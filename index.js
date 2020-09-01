715c3b616a35de6b03ff9f4c40935a6bfa2ac7b5
sanjeevyg <yogi.sjv@gmail.com>
2020-08-31 23:22:08 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+document.body.removeChild(document.querySelector("main"))
+
+const newHeader = document.createElement("h1")
+document.appendChild(newHeader)
+
+// newHeader.id = "victory"
+
+// const victory = document.createElement("#victory")
+// newHeader.appendChild(victory)
+
+const newContent = document.createTextNode("Sanjeev is the champion")
+newHeader.appendChild(newContent)
